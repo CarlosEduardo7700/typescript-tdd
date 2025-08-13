b2dba3c7557393b067a55dac6fd42d022370b179
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-08-12 22:25:05 -0300

    feat: Cria o teste e injeta o ListTasks no ListTasksController

--- src/adapters/controllers/task/listTasks.spec.ts
+++ src/adapters/controllers/task/listTasks.spec.ts
@@ -1,10 +1,60 @@
-import { noContent } from "../../presentations/api/httpResponses/httpResponses";
+import { Task } from "../../../entities/task";
+import { ListTasks } from "../../../usecases/listTasks";
+import { noContent, ok } from "../../presentations/api/httpResponses/httpResponses";
 import { ListTasksController } from "./listTasks";
+
+interface SutTypes {
+  sut: ListTasksController;
+  listTasksStub: ListTasks;
+}
+
+const makeListTasksStub = (): ListTasks => {
+  class ListTasksStub implements ListTasks {
+    async list(): Promise<Task[]> {
+      return Promise.resolve(makeFakeListTasks());
+    }
+  }
+
+  return new ListTasksStub();
+};
+
+const makeSut = (): SutTypes => {
+  const listTasksStub = makeListTasksStub();
+  const sut = new ListTasksController(listTasksStub);
+  return {
+    sut,
+    listTasksStub
+  };
+};
+
+const makeFakeListTasks = (): Task[] => {
+  return [
+    {
+      id: "1",
+      title: "Teste 1",
+      description: "Primeira task para testes.",
+      date: "01/01/2025",
+    },
+    {
+      id: "2",
+      title: "Teste 2",
+      description: "Segunda task para testes.",
+      date: "01/02/2025",
+    },
+  ];
+};
 
 describe("ListTasks Controller", () => {
   test("Retornar 204 se a lista estiver vazia", async () => {
-    const sut = new ListTasksController();
-    const tasks = await sut.handle({});
-    expect(tasks).toEqual(noContent());
+    const { sut, listTasksStub } = makeSut();
+    jest.spyOn(listTasksStub, "list").mockReturnValueOnce(Promise.resolve([]));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(noContent());
+  });
+
+  test("Retornar 200 com uma lista de tarefa", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(ok(makeFakeListTasks()));
   });
 });
