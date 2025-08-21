dd4d03dfcd22959b1f70438d604bb7c54d927034
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-08-20 21:49:02 -0300

    feat: Testa se quando a lista está vazia o retorno é 204

---
+++ src/adapters/factories/listTaskControllerFactory.ts
@@ -0,0 +1,16 @@
+import {
+  LogErrorMongoRepository,
+  TaskMongoRepository,
+} from "../../dataSources";
+import { DbListTasks } from "../../dataSources/db/dbListTasks";
+import { ListTasksController } from "../controllers/task/listTasks";
+import { LogErrorControllerDecorator } from "../decorators/logErrorControllerDecorator";
+import { Controller } from "../interfaces/controller";
+
+export const listTaskControllerFactory = (): Controller => {
+  const taskMongoRepository = new TaskMongoRepository();
+  const dbListTasks = new DbListTasks(taskMongoRepository);
+  const taskController = new ListTasksController(dbListTasks);
+  const logMongoRepository = new LogErrorMongoRepository();
+  return new LogErrorControllerDecorator(taskController, logMongoRepository);
+};
